bee42dd3fd3b9dfa570bcae3ef00dd958d350b85
Pynch <Andrew.Pynch@cppcorp.com>
2020-01-08 07:29:49 -0800

    Added update_git_repos.ps1 to perform equivelant updates on Windows PC's

---
+++ git_automation/update_git_repos.ps1
@@ -0,0 +1,10 @@
+﻿#cd users\apynch\Documents\Github
+Get-ChildItem -Recurse -Depth 2 -Force | 
+ Where-Object { $_.Mode -match "h" -and $_.FullName -like "*\.git" } |
+ ForEach-Object {
+    Write-Host $_.FullName
+    cd $_.FullName
+    cd ../
+    git pull
+    cd ../
+ }
