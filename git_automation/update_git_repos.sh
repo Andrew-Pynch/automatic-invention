6260b85a19f73d03b3a4020d35c96a5b1b039a65
Andrew <andrewpynchbusiness@gmail.com>
2020-01-08 17:33:05 -0800

    small visual change

--- git_automation/update_git_repos.sh
+++ git_automation/update_git_repos.sh
@@ -10,7 +10,7 @@ echo "=====Pulling in latest changes for all repositories====="
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -name ".git" | cut -c 3-); do
     echo "";
-    echo "====="+$i+"=====";
+    echo "====="$i"=====";
 
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
